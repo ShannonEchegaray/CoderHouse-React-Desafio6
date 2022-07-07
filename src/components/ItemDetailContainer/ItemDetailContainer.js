4bd0999e06538826074274850b5c5178648fcfc6
Shannon Echegaray <shannonechegaray@gmail.com>
2022-07-06 21:42:23 -0300

    Agregando promesas

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,37 +1,45 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import ItemDetail from '../ItemDetail/ItemDetail';
 import PulseLoader from "react-spinners/PulseLoader";
 import "./ItemDetailContainer.css"
 
 const ItemDetailContainer = () => {
 
-    const [product, setProduct] = useState({})
+  const [product, setProduct] = useState({})
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(async () => {
-      try {
-        const response = await fetch("./producto.json");
-        const datos = await response.json();
-        setProduct(datos);
-      } catch (error) {
-        setProduct(error)
-        console.log(error)
-      } finally {
+    const getItem = () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(async () => {
+          try {
+            const response = await fetch("./producto.json");
+            const datos = await response.json();
+            resolve(datos)
+          } catch (error) {
+            reject(error)
+          }  
+        });
+      }, 2000);
+        
+    }
+
+    getItem()
+      .then(items => {
+        setProduct(items);
+        console.log(items);
         setLoading(false);
-      }
-
-    }, 2000)
-
+      })
+      .catch(error => setProduct(new Error(error)))
+    
   }, []);
 
   return (
     <div className='ItemDetailContainer'>
-        {loading
-              ? (<><h2>Cargando...</h2>
-                <PulseLoader size={10} color="#157A6E" cssOverride={{margin: "2em"}} /></>)
-              : <ItemDetail item={product}/>}
-        
+      {loading
+        ? (<><h2>Cargando...</h2>
+          <PulseLoader size={10} color="#157A6E" cssOverride={{ margin: "2em" }} /></>)
+        :<ItemDetail item={product} />}
     </div>
   )
 }
